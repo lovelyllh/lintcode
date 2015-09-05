de6e264f7ad891d21afad9eba985c145ae078c98
りんちゃん <lovelyllh@gmail.com>
2015-09-05 01:29:49 -0400

    Update FirstBadVersion.java

--- 2-binarySearchAndSortedArray/FirstBadVersion.java
+++ 2-binarySearchAndSortedArray/FirstBadVersion.java
@@ -1,9 +1,6 @@
 package binarySearchAndSortedArray;
 /*
 first bad version:
-question: http://www.lintcode.com/en/problem/first-bad-version/
-Answer: http://www.jiuzhang.com/solutions/first-bad-version/
-
 The code base version is an integer start from 1 to n. One day, someone committed a bad version in the code case, 
 so it caused this version and the following versions are all failed in the unit tests. Find the first bad version.
 You can call isBadVersion to help you determine which version is the first bad one. The details interface can be found 
