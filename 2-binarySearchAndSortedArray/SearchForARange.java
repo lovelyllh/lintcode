7d48318ef50c19d8dc8bdf2db590e1c8aa93369a
りんちゃん <lovelyllh@gmail.com>
2015-09-05 01:31:58 -0400

    Update SearchForARange.java

--- 2-binarySearchAndSortedArray/SearchForARange.java
+++ 2-binarySearchAndSortedArray/SearchForARange.java
@@ -3,8 +3,6 @@ package binarySearchAndSortedArray;
 import java.util.ArrayList;
 
 /*Search for a Range
-question:http://www.lintcode.com/en/problem/search-for-a-range/#
-answer:  http://www.jiuzhang.com/solutions/search-for-a-range/
 Given a sorted array of n integers, find the starting and ending position of a given target value.
 If the target is not found in the array, return [-1, -1].
 Example
