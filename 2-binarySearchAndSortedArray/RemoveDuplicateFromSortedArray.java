06248ad43727dcf51073ef9004f7c5dc05d724d6
りんちゃん <lovelyllh@gmail.com>
2015-09-05 01:31:27 -0400

    Update RemoveDuplicateFromSortedArray.java

--- 2-binarySearchAndSortedArray/RemoveDuplicateFromSortedArray.java
+++ 2-binarySearchAndSortedArray/RemoveDuplicateFromSortedArray.java
@@ -1,9 +1,6 @@
 package binarySearchAndSortedArray;
 
 /*remove Duplicates from Sorted Array
-qustion: http://www.lintcode.com/en/problem/remove-duplicates-from-sorted-array/
-answer:  http://www.jiuzhang.com/solutions/search/?question=Array
-
 Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 Do not allocate extra space for another array, you must do this in place with constant memory.
 
