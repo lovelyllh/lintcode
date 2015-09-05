e799aafcfa799e05f1cdebf3ee0b1c1bef203bc7
りんちゃん <lovelyllh@gmail.com>
2015-09-05 01:31:14 -0400

    Update RecoverRotatedSortedArray.java

--- 2-binarySearchAndSortedArray/RecoverRotatedSortedArray.java
+++ 2-binarySearchAndSortedArray/RecoverRotatedSortedArray.java
@@ -1,8 +1,5 @@
 /*Recover Rotated Sorted Array
-Question: http://www.lintcode.com/en/problem/recover-rotated-sorted-array/
-
 Given a rotated sorted array, recover it to sorted array in-place.
-
 example
 [4, 5, 1, 2, 3] -> [1, 2, 3, 4, 5]
 Challenge
@@ -11,10 +8,6 @@ In-place, O(1) extra space and O(n) time.
 Clarification
 What is rotated array?
 For example, the orginal array is [1,2,3,4], The rotated array of it can be [1,2,3,4], [2,3,4,1], [3,4,1,2], [4,1,2,3]
-
-Answer:
-http://www.jiuzhang.com/solutions/recover-rotated-sorted-array/
-
 */
 package binarySearchAndSortedArray;
 
