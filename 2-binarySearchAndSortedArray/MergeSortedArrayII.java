bb4556f03e9cdc695bba7dda820a7b028e3f753f
りんちゃん <lovelyllh@gmail.com>
2015-09-05 01:30:57 -0400

    Update MergeSortedArrayII.java

--- 2-binarySearchAndSortedArray/MergeSortedArrayII.java
+++ 2-binarySearchAndSortedArray/MergeSortedArrayII.java
@@ -1,6 +1,4 @@
 /*Merge Sorted Array II
-Question: http://www.lintcode.com/en/problem/merge-sorted-array-ii/#
-
 Merge two given sorted integer array A and B into a new sorted integer array.
 Example
 A=[1,2,3,4]
@@ -9,9 +7,6 @@ return [1,2,2,3,4,4,5,6]
 
 Challenge
 How can you optimize your algorithm if one array is very large and the other is very small?
-
-Answer: http://www.jiuzhang.com/solutions/merge-sorted-array-ii/
-
 */
 
 
