2b92abe7a62a0ececc54012649aba733e834ffa4
りんちゃん <lovelyllh@gmail.com>
2015-09-05 01:32:10 -0400

    Update SearchInRoatedSortedArray.java

--- 2-binarySearchAndSortedArray/SearchInRoatedSortedArray.java
+++ 2-binarySearchAndSortedArray/SearchInRoatedSortedArray.java
@@ -1,9 +1,6 @@
 package binarySearchAndSortedArray;
 
 /*Search in Rotated Sorted Array
-question: http://www.lintcode.com/en/problem/search-in-rotated-sorted-array/
-answer: http://www.jiuzhang.com/solutions/search-in-rotated-sorted-array/
-
 Suppose a sorted array is rotated at some pivot unknown to you beforehand.
 (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
 You are given a target value to search. If found in the array return its index, otherwise return -1.
